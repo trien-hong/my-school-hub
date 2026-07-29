c262f905555a4228e92fe58974b0251247feeffe
trien-hong <danielhong55@gmail.com>
2026-07-29 19:00:56 -0400

    implemented JWT, updated requirements.txt, added basic user model, URLs, serializers, and views

--- backend/system-management/blueprint/settings.py
+++ backend/system-management/blueprint/settings.py
@@ -14,6 +14,7 @@ import os
 
 from pathlib import Path
 from dotenv import load_dotenv
+from datetime import timedelta
 
 load_dotenv()
 
@@ -48,8 +49,23 @@ INSTALLED_APPS = [
     'user',
 
     # third party apps
-    'rest_framework'
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -110,6 +126,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'user.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
---
+++ backend/system-management/user/API/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from user.API.views import LogoutView, RegisterView, UserProfileView
+
+urlpatterns = [
+    # JWT endpoints
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/logout/', LogoutView.as_view(), name='logout'),
+
+    # User endpoints
+    path('auth/register/', RegisterView.as_view()),
+    path('auth/profile/', UserProfileView.as_view()),
+]
