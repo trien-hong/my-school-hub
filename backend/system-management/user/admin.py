8c4204bab08a33db8fcb31afd603486f42210bc3
trien-hong <danielhong55@gmail.com>
2026-07-30 01:01:18 -0400

    updated admin.py in user app to display all users

--- backend/system-management/user/admin.py
+++ backend/system-management/user/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
+
+    list_filter = ('groups', 'is_staff', 'is_superuser', 'is_active')
+
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+
+    ordering = ('id',)
