8925e38cd09b6304cc5747732131a3d9c5d87634
trien-hong <danielhong55@gmail.com>
2026-08-03 18:06:41 -0400

    updated admin.py to include UserInvitation

--- backend/system-management/user/admin.py
+++ backend/system-management/user/admin.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
+from user.models import UserInvitation
 
 User = get_user_model()
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
-
     list_filter = ('groups', 'is_staff', 'is_superuser', 'is_active')
-
     search_fields = ('username', 'first_name', 'last_name', 'email')
-
     ordering = ('id',)
+
+@admin.register(UserInvitation)
+class UserInvitationAdmin(admin.ModelAdmin):
+    list_display = ('invite_code', 'group_role', 'is_used', 'created_by', 'created_at')
+    list_filter = ('group_role', 'is_used', 'created_at')
+    search_fields = ('invite_code', 'created_by__username')
+    readonly_fields = ('invite_code', 'created_at')
